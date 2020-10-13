81a7ad7b595867dc33040bdd8fa9e73b326e16ab
zhaosw <zhaosw@outlook.com>
2020-10-13 23:48:13 +0800

    添加项目文件。

---
+++ src/SPACore.PhoneBook.Core/PhoneBooks/PhoneNumbers/PhoneNumber.cs
@@ -0,0 +1,35 @@
+﻿using Abp.Domain.Entities;
+using Abp.Domain.Entities.Auditing;
+using SPACore.PhoneBook.PhoneBooks.Persons;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace SPACore.PhoneBook.PhoneBooks.PhoneNumbers
+{
+    /// <summary>
+    /// 电话号码
+    /// </summary>
+    public class PhoneNumber : Entity<long>, IHasCreationTime
+    {
+        /// <summary>
+        /// 电话号码
+        /// </summary>
+        [Required]
+        [MaxLength(11)]
+        public string Number { get; set; }
+        /// <summary>
+        /// 创建时间
+        /// </summary>
+        public DateTime CreationTime { get; set; }
+        /// <summary>
+        /// 类型
+        /// </summary>
+
+        public PhoneNumberType PhoneType { get; set; }
+        public int PersonId { get; set; }
+
+        public Person Person { get; set; }
+    }
+}
---
+++ src/SPACore.PhoneBook.Core/PhoneBooks/PhoneNumbers/PhoneNumberType.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SPACore.PhoneBook.PhoneBooks.PhoneNumbers
+{
+    /// <summary>
+    /// 号码类型
+    /// </summary>
+    public enum PhoneNumberType
+    {
+        /// <summary>
+        /// 手机
+        /// </summary>
+        Mobile = 1,
+        /// <summary>
+        /// 座机
+        /// </summary>
+        Home,
+        /// <summary>
+        /// 公司
+        /// </summary>
+        Cpmpany
+    }
+}
